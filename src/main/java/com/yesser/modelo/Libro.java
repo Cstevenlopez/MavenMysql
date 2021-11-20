6ebb1a3b6c593008344c47e00c75b1d935de38cd
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-11-20 11:23:13 -0600

    Creacion del modelo libro

---
+++ src/main/java/com/yesser/modelo/Libro.java
@@ -0,0 +1,20 @@
+package com.yesser.modelo;
+
+public class Libro {
+    private String titulo;
+
+    public Libro() {
+    }
+
+    public Libro(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}
